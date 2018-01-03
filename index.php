b5cc0b3214e232898b6cd8c2f302ea9b89c6529b
kalsan1998 <kaan.alsan@live.com>
2018-01-03 15:51:34 -0500

    added github link

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <title>Kaan's Chat Room</title>
     <link rel="stylesheet" href="chatStyle.css"/>
+    <link rel="icon" href="img/chat.ico"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="chatRoom.js"></script>
 
 </head>
 <body>
+    <div id="github">
+        <a href="https://github.com/kalsan1998/ChatRoom"><img id="githubIcon" src="img/github-2048-black.png"/>GitHub Link</a>
+    </div>
+
     <div id="container">
 
         <h2 id="header">Chat Room Made with jQuery and PHP</h2>
